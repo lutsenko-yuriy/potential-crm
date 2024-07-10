0addf11b1ae0b58a8dae8ef0fb4776f1d212c8b7
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-11 01:54:11 +0200

    Add get student by id method

--- src/main/kotlin/com/iurii/demo/rest/StudentRestController.kt
+++ src/main/kotlin/com/iurii/demo/rest/StudentRestController.kt
@@ -1,8 +1,10 @@
 package com.iurii.demo.rest
 
 import com.iurii.demo.entity.Student
+import jakarta.annotation.PostConstruct
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,15 +12,31 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class StudentRestController {
 
-    @GetMapping("/students")
-    fun students(): ResponseEntity<List<Student>> {
-        return ResponseEntity.ok(
+    val students = mutableListOf<Student>()
+
+    @PostConstruct
+    fun initStudentsList() {
+        students.addAll(
             listOf(
                 Student("Jack", "Michael"),
                 Student("John", "Doe"),
                 Student("John", "Smith")
             )
         )
+    }
+
+    @GetMapping("/students")
+    fun students(): ResponseEntity<List<Student>> {
+        return ResponseEntity.ok(students)
+    }
+
+    @GetMapping("/students/{id}")
+    fun student(@PathVariable id: Int): ResponseEntity<Student> {
+        return if (id in students.indices) {
+            ResponseEntity.ok(students[id])
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 
 }
