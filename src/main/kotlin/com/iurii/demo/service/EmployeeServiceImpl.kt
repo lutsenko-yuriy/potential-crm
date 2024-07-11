fa865de2c34c2a4d9428c0ffa4098b549bade1d5
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-11 19:45:50 +0200

    Update Employee service

--- src/main/kotlin/com/iurii/demo/service/EmployeeServiceImpl.kt
+++ src/main/kotlin/com/iurii/demo/service/EmployeeServiceImpl.kt
@@ -1,11 +1,29 @@
 package com.iurii.demo.service
 
 import com.iurii.demo.dao.EmployeeDao
+import com.iurii.demo.entity.Employee
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class EmployeeServiceImpl(private val employeeDao: EmployeeDao) : EmployeeService {
 
-    override fun findAll() = employeeDao.findAll()
+    override fun findAll(): List<Employee> {
+        return employeeDao.findAll()
+    }
+
+    override fun findById(id: Long): Employee {
+        return employeeDao.findById(id)
+    }
+
+    @Transactional
+    override fun save(employee: Employee): Employee {
+        return employeeDao.save(employee)
+    }
+
+    @Transactional
+    override fun deleteById(id: Long) {
+        employeeDao.deleteById(id)
+    }
 
 }
