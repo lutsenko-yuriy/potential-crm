1b762014a15fba3e49ce600033ea78f1410374db
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-11 21:31:57 +0200

    Play with configs of data rest

--- src/main/kotlin/com/iurii/demo/dao/EmployeeRepository.kt
+++ src/main/kotlin/com/iurii/demo/dao/EmployeeRepository.kt
@@ -2,7 +2,7 @@ package com.iurii.demo.dao
 
 import com.iurii.demo.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@Repository
+@RepositoryRestResource(path = "members")
 interface EmployeeRepository: JpaRepository<Employee, Long>
