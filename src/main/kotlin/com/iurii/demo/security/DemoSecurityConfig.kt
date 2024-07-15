092c02bc34d2f0cd3ea4caf7e8ae105f1e5458da
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 11:18:01 +0200

    Adapt security to MVC

--- src/main/kotlin/com/iurii/demo/security/DemoSecurityConfig.kt
+++ src/main/kotlin/com/iurii/demo/security/DemoSecurityConfig.kt
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import javax.sql.DataSource
 
 @Configuration
+@EnableWebSecurity
 class DemoSecurityConfig {
 
     @Bean
@@ -21,6 +23,7 @@ class DemoSecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                 .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
                 .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
         }
 
         http.httpBasic(Customizer.withDefaults())
