c7e50d875a581eabcfa7e62a1ccc65a6a3ced81d
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 11:49:32 +0200

    Add form and controller to add new employee

--- src/main/kotlin/com/iurii/demo/controller/EmployeeController.kt
+++ src/main/kotlin/com/iurii/demo/controller/EmployeeController.kt
@@ -1,20 +1,36 @@
 package com.iurii.demo.controller
 
+import com.iurii.demo.entity.Employee
 import com.iurii.demo.service.EmployeeService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/employees")
 class EmployeeController(val employeeService: EmployeeService) {
 
-    @GetMapping("/list")
+    @GetMapping(value = ["/", "/list"])
     fun listOfEmployees(model: Model): String {
         val employees = employeeService.findAll()
         model.addAttribute("employees", employees)
         return "employees/list"
+    }
+
+    @GetMapping("/add")
+    fun addEmployee(model: Model): String {
+        val employee = Employee()
+        model.addAttribute("employee", employee)
+        return "employees/add"
+    }
+
+    @PostMapping("/save")
+    fun saveEmployee(@ModelAttribute("employee") employee: Employee): String {
+        employeeService.save(employee)
+        return "redirect:/employees/list"
     }
 
 }
