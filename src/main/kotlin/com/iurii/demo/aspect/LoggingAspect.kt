8158ee474c73fba98651569dae1ade2e1cb47c12
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-18 11:16:39 +0200

    Add the advice

--- src/main/kotlin/com/iurii/demo/aspect/LoggingAspect.kt
+++ src/main/kotlin/com/iurii/demo/aspect/LoggingAspect.kt
@@ -1,6 +1,8 @@
 package com.iurii.demo.aspect
 
+import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
 import java.util.logging.Logger
@@ -23,4 +25,9 @@ class LoggingAspect {
     @Pointcut("forControllers() || forServices() || forDAOs()")
     fun forNecessaryMethods() {}
 
+    @Before("forNecessaryMethods()")
+    fun before(joinPoint: JoinPoint) {
+        logger.info("=======> Before running the method ${joinPoint.signature.toShortString()}")
+        logger.info("=======> With arguments: ${joinPoint.args.contentToString()}")
+    }
 }
