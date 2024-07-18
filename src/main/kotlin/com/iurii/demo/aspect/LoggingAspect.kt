71d32c562fafd881c72648c1e2d5ae6fb739f542
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-18 11:20:14 +0200

    Add the after advice

--- src/main/kotlin/com/iurii/demo/aspect/LoggingAspect.kt
+++ src/main/kotlin/com/iurii/demo/aspect/LoggingAspect.kt
@@ -1,6 +1,7 @@
 package com.iurii.demo.aspect
 
 import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.AfterReturning
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
@@ -29,5 +30,12 @@ class LoggingAspect {
     fun before(joinPoint: JoinPoint) {
         logger.info("=======> Before running the method ${joinPoint.signature.toShortString()}")
         logger.info("=======> With arguments: ${joinPoint.args.contentToString()}")
+    }
+
+    @AfterReturning(pointcut = "forNecessaryMethods()", returning = "result")
+    fun afterReturning(joinPoint: JoinPoint, result: Any?) {
+        logger.info("=======> After running the method ${joinPoint.signature.toShortString()}")
+        logger.info("=======> With arguments: ${joinPoint.args.contentToString()}")
+        logger.info("=======> The result is: ${result.toString()}")
     }
 }
